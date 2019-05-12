0dcf184348334323fe8e026a34c050305b46ccf9
Shuo <anshuobuaa@gmail.com>
2019-05-12 16:39:56 +0100

    add three hidden layer MLP

--- mlp.py
+++ mlp.py
@@ -31,6 +31,38 @@ class MLPNetwork(nn.Module):
         return F.log_softmax(out, dim=1)
 
 
+class MLPNetwork_3hidden(nn.Module):
+    '''
+    Parameters:
+        n_features: the input features number.
+        n_neurons: [x1,x2,x3] Three hidden layers and the number of neurons in each layer.
+    Return:
+        softmax probability.
+    '''
+
+    def __init__(self, n_features, n_neurons, dropouts):
+        super().__init__()
+        self.layer1 = nn.Linear(in_features=n_features,
+                                out_features=n_neurons[0])
+        self.dropout1 = nn.Dropout(dropouts[0])
+        self.layer2 = nn.Linear(
+            in_features=n_neurons[0], out_features=n_neurons[1])
+        self.dropout2 = nn.Dropout(dropouts[1])
+        self.layer3 = nn.Linear(
+            in_features=n_neurons[1], out_features=n_neurons[2])
+        self.dropout3 = nn.Dropout(dropouts[2])
+        self.out_layer = nn.Linear(in_features=n_neurons[2], out_features=4)
+
+    def forward(self, X):
+        out = F.relu(self.layer1(X))
+        out = self.dropout1(out)
+        out = F.relu(self.layer2(out))
+        out = self.dropout2(out)
+        out = F.relu(self.layer3(out))
+        out = self.dropout3(out)
+        out = self.out_layer(out)
+        return F.log_softmax(out, dim=1)
+
 # Note:
 # The loss used in NLLLoss, can have weights
 # It's very useful when you train the unbalanced data set.
